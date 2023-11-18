338efb013fdff507e5e222a4cfb7aff260306fbf
joshhunt <josh@trtr.co>
2023-11-18 15:07:59 +0000

    simpler hello world

--- commands/hello.ts
+++ commands/hello.ts
@@ -12,25 +12,8 @@ const lambda = new AWS.Lambda();
 export default async function handleHelloCommand(
   command: APIApplicationCommandInteraction
 ): Promise<APIInteractionResponse> {
-  const lambdaARN = process.env.SELF_LAMBDA_ARN;
-  if (!lambdaARN) {
-    return respond(
-      "Unable to start the server because process.env.SELF_LAMBDA_ARN isn't set!"
-    );
-  }
-
-  await lambda
-    .invokeAsync({
-      FunctionName: lambdaARN,
-      InvokeArgs: JSON.stringify({
-        command: "say Hello, world!",
-        discordToken: command.token,
-      }),
-    })
-    .promise();
-
   return {
     type: InteractionResponseType.ChannelMessageWithSource,
-    data: { content: `Hello, world! Invoked ${lambdaARN}` },
+    data: { content: `Hello, world!` },
   };
 }
